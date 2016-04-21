e9ac2aca6523d211b242962835f48426da56388f
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-04-21 23:38:03 +0900

    Maps クラス実装

--- src/main/kotlin/gl8080/lifegame/util/Maps.kt
+++ src/main/kotlin/gl8080/lifegame/util/Maps.kt
@@ -1,13 +1,26 @@
 package gl8080.lifegame.util
 
-import java.util.*
+class Maps_<K, V> private constructor() {
 
-class Maps_<K, V> {
+    companion object MapsObject_ {
+
+        fun <K, V> map(key: K, value: V) : Map<K, V> {
+            return Maps_<K, V>().put(key, value).get()
+        }
+
+        fun <K, V> newMap() : Maps_<K, V> {
+            return Maps_()
+        }
+    }
 
     private val map : MutableMap<K, V> = mutableMapOf()
 
     fun put(key: K, value: V) : Maps_<K, V> {
         this.map.put(key, value)
         return this
+    }
+
+    fun get() : Map<K, V> {
+        return this.map
     }
 }
