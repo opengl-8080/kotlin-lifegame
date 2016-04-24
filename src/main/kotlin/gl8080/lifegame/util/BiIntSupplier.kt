60347893c0a76c33dc66fc00e8eaafa85d0f6f46
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-04-24 22:15:32 +0900

    BiIntSupplier

--- src/main/kotlin/gl8080/lifegame/util/BiIntSupplier.kt
+++ src/main/kotlin/gl8080/lifegame/util/BiIntSupplier.kt
@@ -1,10 +1,11 @@
-package gl8080.lifegame.util;
+package gl8080.lifegame.util
+
 
 /**
  * 2つのint値オペランドを受け取って結果を返さないオペレーションを表します。
  */
 @FunctionalInterface
-public interface BiIntSupplier<T> {
-    
-    T get(int i, int j);
+interface BiIntSupplier<T> {
+
+    fun get(i: Int, j: Int): T
 }
