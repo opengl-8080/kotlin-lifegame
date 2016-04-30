e763b83d00329501be877c605479b2c717715654
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-04-30 16:15:01 +0900

    AbstractEntity

---
+++ src/main/kotlin/gl8080/lifegame/logic/AbstractEntity.kt
@@ -0,0 +1,40 @@
+package gl8080.lifegame.logic
+
+import java.io.Serializable
+import java.util.*
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class AbstractEntity : Serializable {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private var id: Long? = null
+
+    constructor()
+
+    fun getId(): Long? {
+        return this.id
+    }
+
+    override fun equals(o: Any?): Boolean {
+        if (this === o) return true
+        if (!(o is AbstractEntity)) return false
+
+        if (o.id == null) {
+            return false
+        } else {
+            return this.id == o.id
+        }
+    }
+
+    override fun hashCode() : Int {
+        if (this.id == null) {
+            return super.hashCode()
+        } else {
+            return Objects.hashCode(this.id)
+        }
+    }
+}
