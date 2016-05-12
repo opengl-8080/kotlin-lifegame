cddbdd5080542088ccf95f58b7553306a0a0aab8
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-12 22:37:32 +0900

    GameDefinitionRepository

--- src/main/kotlin/gl8080/lifegame/logic/definition/GameDefinitionRepository.kt
+++ src/main/kotlin/gl8080/lifegame/logic/definition/GameDefinitionRepository.kt
@@ -1,37 +1,36 @@
-package gl8080.lifegame.logic.definition;
-
-import gl8080.lifegame.logic.exception.NotFoundEntityException;
+package gl8080.lifegame.logic.definition
 
 /**
  * ゲーム定義の永続化を行うリポジトリ。
  */
-public interface GameDefinitionRepository {
-    
+interface GameDefinitionRepository {
+
     /**
      * ゲーム定義を登録する。
      * @param gameDefinition 登録するゲーム定義
      */
-    void register(GameDefinition gameDefinition);
-    
+    fun register(gameDefinition: GameDefinition)
+
     /**
      * 指定した ID のゲーム定義を検索する。
      * @param id ID
      * @return ゲーム定義
      * @throws NotFoundEntityException ID に紐づくゲーム定義が存在しない場合
      */
-    GameDefinition search(long id);
-    
+    fun search(id: Long): GameDefinition
+
     /**
      * 指定した ID のゲーム定義を検索し、排他ロックする。
      * @param id ID
      * @return ゲーム定義
      * @throws NotFoundEntityException ID に紐づくゲーム定義が存在しない場合
      */
-    GameDefinition searchWithLock(long id);
-    
+    fun searchWithLock(id: Long): GameDefinition
+
     /**
      * 指定したゲーム定義を削除する。
      * @param gameDefinition 削除するゲーム定義
      */
-    void remove(GameDefinition gameDefinition);
+    fun remove(gameDefinition: GameDefinition)
 }
+
