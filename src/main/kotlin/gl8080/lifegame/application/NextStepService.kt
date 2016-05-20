8ec644ac234338c50e477300374f092e57aed8ad
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-20 23:35:18 +0900

    NextStepService 途中

---
+++ src/main/kotlin/gl8080/lifegame/application/NextStepService.kt
@@ -0,0 +1,25 @@
+package gl8080.lifegame.application
+
+import gl8080.lifegame.logic.Game
+import gl8080.lifegame.logic.GameRepository
+import org.slf4j.Logger
+import javax.ejb.Stateless
+import javax.inject.Inject
+
+@Stateless
+open class NextStepService_ {
+    @field:Inject
+    lateinit private var logger: Logger
+    @field:Inject
+    lateinit private var repository: GameRepository
+
+    fun next(id: Long): Game {
+        this.logger.info("next step game (id={})", id)
+
+        val game = this.repository.searchWithLock(id)
+        game.initializeNeighborCells()
+        game.nextStep()
+
+        return game
+    }
+}
