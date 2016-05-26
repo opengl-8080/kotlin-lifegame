4628dc4fa3be9b35de127c133b3699c046812aca
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-05-26 23:40:10 +0900

    MyApplication

--- src/main/java/gl8080/lifegame/web/MyApplication.java
+++
@@ -1,8 +0,0 @@
-package gl8080.lifegame.web;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class MyApplication extends Application {
-}
---
+++ src/main/kotlin/gl8080/lifegame/web/MyApplication.kt
@@ -0,0 +1,7 @@
+package gl8080.lifegame.web
+
+import javax.ws.rs.ApplicationPath
+import javax.ws.rs.core.Application
+
+@ApplicationPath("/api")
+class MyApplication: Application()
